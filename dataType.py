bf07f00ce8c9a2d43bd0760f12731f2bfd346335
kurniawan2805 <42286406+kurniawan2805@users.noreply.github.com>
2018-11-26 08:56:23 +0700

    Create dataType.py
    mix integer, float, and string

---
+++ dataType.py
@@ -0,0 +1,21 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+i2 = 7
+d2 = 7.0
+s2 = 'Adi '
+# Read and save an integer, double, and String to your variables.
+i3=int(input())
+d3=float(input())
+s3=input()
+# Print the sum of both integer variables on a new line.
+iSum=i+i3
+print (iSum)
+# Print the sum of the double variables on a new line.
+dSum=d+d3
+print(dSum)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+sSum=s+s3
+print(sSum)
